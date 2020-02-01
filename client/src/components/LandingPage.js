33a74f8a5946341ea75f416a946d5d7d3337ef22
dasturi <oba93@hotmail.com>
2020-02-01 19:26:11 +0100

    landing page css

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,38 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Landing.css';
 
 const LandingPage = () => {
   return (
-    <>
-      <div className='container' style={ { marginLeft:'0%', marginTop:'10%', textAlign:'center' } }>
-        <div className='row'>
-          <div className= 'col-md-12'>
-            <div className = "landing-welcome">
-              <h1>WELCOME </h1>
-              <h3>Upload and Retrieve forms data from mongodb using MERN stack</h3>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='container' style={ { marginLeft:'0%', marginTop:'1%', textAlign:'center' } }>
-        <div className='row'>    
-          <div className= 'col-md-12'>
-            <div style={ { border: '1px solid grey', display: 'inline-block', padding: '2px' } }>
-              <button className='btn'> <NavLink to= '/shortForm'>Submit Form</NavLink></button> 
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='container' style={ { marginLeft:'0%', marginTop:'1%', textAlign:'center' } }>
-        <div className='row'>
-          <div className= 'col-md-12'>
-            <div style={ { border: '1px solid grey', display: 'inline-block', padding: '2px', alignSelf:'center' } }>
-              <button className='btn'><NavLink to= '/trackcomplaint'>Edit Form</NavLink> </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="Landing">
+      <h1 className="Landing-item">WELCOME </h1>
+      <h3 className="Landing-item">Upload and Retrieve forms data from mongodb using MERN stack</h3>
+      <button className='btn'> <NavLink to= '/shortForm'>Submit Form</NavLink></button>
+      <button className='btn'><NavLink to= '/trackcomplaint'>Edit Form</NavLink> </button>
+    </div>
   );
 };
 export default LandingPage;
