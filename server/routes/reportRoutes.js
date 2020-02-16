f0416b8767154c26ec29d56eb6786115fc8677be
dasturi <oba93@hotmail.com>
2020-02-16 20:40:56 +0100

    find form test

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,17 +1,16 @@
 /* eslint-disable no-unused-vars */
-const express = require('express');
+const express = require("express");
 const reportRoutes = express.Router();
-const Forms = require('../models/schema');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const crypto = require('crypto');
+const Forms = require("../models/schema");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const crypto = require("crypto");
 const uri = process.env.ATLAS_URI;
 
 // Create storage engine
 const storage = new GridFsStorage({
   url: uri,
   file: (req, file) => {
-		
     return new Promise((resolve, reject) => {
       crypto.randomBytes(16, (err, buf) => {
         if (err) {
@@ -20,65 +19,65 @@ const storage = new GridFsStorage({
         const filename = file.originalname;
         const fileInfo = {
           filename: filename,
-          bucketName: 'uploads'
+          bucketName: "uploads"
         };
         resolve(fileInfo);
       });
     });
   }
 });
-  
+
 const upload = multer({ storage });
 
 // Add FORM
-reportRoutes.post('/add', upload.array('multiple_images'), (req, res) => { 
-	
+reportRoutes.post("/add", upload.array("multiple_images"), (req, res) => {
   if (req.fileValidationError) {
-    return res.status(400).json({ 'File upload Error': req.fileValidationError });
-  }
-  else if (!req.files) {
+    return res
+      .status(400)
+      .json({ "File upload Error": req.fileValidationError });
+  } else if (!req.files) {
     // console.log({error:'Please select an image to upload'});
   }
-		
+
   const data = req.body;
-  data['attachments'] = req.files;
-		
+  data["attachments"] = req.files;
+
   const addTodo = new Forms(data);
-		
-  addTodo.save()
+
+  addTodo
+    .save()
     .then(reqq => {
       // console.log('form added ', reqq);
-				
-      return res.status(200).json({ 'id': reqq._id });
+
+      return res.status(200).json({ id: reqq._id });
     })
     .catch(err => {
       // console.log('form NOT added ', err);
-      return res.status(400).json({ 'Error': err });
+      return res.status(400).json({ Error: err });
     });
-	
 });
 
 // FIND FORM BY ID
-reportRoutes.get('/findone/:id', async(req, res) => {
+reportRoutes.get("/findone/:id", async (req, res) => {
   const { id } = req.params;
 
   //await Forms.findOne({'_id': id}, (err, data) => err ? res.sendStatus(400) : res.json(data));
-  await Forms.findOne({ '_id': id })
+  await Forms.findOne({ _id: id })
     .then(reqq => {
-      if(reqq) {
+      if (reqq) {
         // console.log('form found ', reqq);
         res.json(reqq);
       } else {
-        res.sendStatus(400);
+        res.status(400).json({ message: "form not found" });
       }
     })
     .catch(err => {
-      res.sendStatus(400);
-    });  	
+      res.status(400).json({ message: "form not found", error: err });
+    });
 });
 
 // Edit form
-reportRoutes.put('/edit/:id', async(req, res) => {
+reportRoutes.put("/edit/:id", async (req, res) => {
   const { id } = req.params;
 
   await Forms.findByIdAndUpdate(id, req.body)
@@ -93,10 +92,10 @@ reportRoutes.put('/edit/:id', async(req, res) => {
 });
 
 // Delete form
-reportRoutes.delete('/delete/:id', async (req, res) => {
+reportRoutes.delete("/delete/:id", async (req, res) => {
   const { id } = req.params;
   // console.log('Id of form to be DELETED', id);
-	
+
   await Forms.findByIdAndDelete(id)
     .then(reqq => {
       // console.log('form deleted ', reqq);
@@ -109,8 +108,8 @@ reportRoutes.delete('/delete/:id', async (req, res) => {
 });
 
 // Get allData
-reportRoutes.get('/allData', (req, res)=> {
-  Forms.find((err, data) => err ? res.sendStatus(400) : res.json(data));
+reportRoutes.get("/allData", (req, res) => {
+  Forms.find((err, data) => (err ? res.sendStatus(400) : res.json(data)));
 });
 
 module.exports = reportRoutes;
--- server/test.js
+++ server/test.js
@@ -1,30 +1,69 @@
-const assert = require('assert');
-const request = require('supertest');
-const api = require('./server');
+const assert = require("assert");
+const request = require("supertest");
+const api = require("./server");
 const app = api.app;
 
-describe('Report System', () => {
-  describe('Erroneous URL', () => {
-    it('returns 404', done => {
+describe("Forms-MERN-stack", () => {
+  describe("Erroneous URL", () => {
+    it("returns 404", done => {
       request(app)
-        .get('/error')
+        .get("/error")
         .expect(404, done);
     });
   });
 
-  describe('GET All data', () => {
-    it('returns 200 OK', done => {
+  describe("GET All data", () => {
+    it("returns 200 OK", done => {
       request(app)
-        .get('/report/allData')
-        .set('Accept', 'application/json')
+        .get("/report/allData")
+        .set("Accept", "application/json")
         .expect(200, done);
     });
 
-    it('returns JSON', done => {
+    it("returns JSON", done => {
       request(app)
-        .get('/report/allData')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/, done);
+        .get("/report/allData")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/, done);
+    });
+  });
+
+  describe("Find one form in the database", () => {
+    it("returns 200", done => {
+      request(app)
+        .get("/report/findone/5e1a4a46a2965e47bbb14d88")
+        .set("Accept", "application/json")
+        .expect(200, done);
+    });
+
+    it("returns json", done => {
+      request(app)
+        .get("/report/findone/5e1a4a46a2965e47bbb14d88")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/, done);
+    });
+
+    it("returns form data", done => {
+      request(app)
+        .get("/report/findone/5e1a4a46a2965e47bbb14d88")
+        .set("Accept", "application/json")
+        .expect(res => {
+          assert.equal(res.body.username, "luci");
+        })
+        .end(done);
+    });
+  });
+
+  describe("GET one failures", () => {
+    it("returns 404 for a FORM that doesnt exist", done => {
+      request(app)
+        .get("/report/findone/UUUUUUUUUU")
+        .set("Accept", "application/json")
+        .expect(res => {
+          // console.log('notf founf', res.body);
+          assert.equal(res.body.message, "form not found");
+        })
+        .end(done);
     });
   });
 });
