118634e8a9c5542d54c36da3be21d9dfcff855c8
dasturi <oba93@hotmail.com>
2020-02-27 19:59:38 +0100

    test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom'
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ReactDOM from "react-dom";
+import App from "./App";
+import FormSmall from "./components/pages/FormSmall";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-test('renders home link', () => {
+test("renders home link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/home/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders submit form link', () => {
+test("renders submit form link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Submit form/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('render edit form link', () => {
-  const  {getByText} = render(<App />);
+test("render edit form link", () => {
+  const { getByText } = render(<App />);
   const linkElement = getByText(/edit form/i);
-  expect(linkElement).toBeInTheDocument(); 
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("render fill in details", () => {
+  const { getByText } = render(<FormSmall />);
+  const linkElement = getByText(/Fill in details/i);
+  expect(linkElement).toBeInTheDocument();
 });
